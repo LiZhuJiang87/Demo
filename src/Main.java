b3dae894a8ba7490b22310febd38c334d4fcae3f
lizhu <li_jiang@aim.com>
2024-04-02 12:52:25 -0500

    add comments and format the code

--- src/Main.java
+++ src/Main.java
@@ -4,13 +4,13 @@ import java.util.Objects;
 public class Main {
     public static void main(String[] args) {
 
-        int[] sortedNumber = {2,7,11,15};
+        int[] sortedNumber = {2, 7, 11, 15};
         int target = 9;
 
-        int[] actual = Utility.findTwoNumbers(sortedNumber,target);
-        int[] expected = new int[]{0,1};
+        int[] actual = Utility.findTwoNumbers(sortedNumber, target);
+        int[] expected = new int[]{0, 1};
 
         System.out.printf("Is the actual equal to the expected? %s%n",
-                           Arrays.equals(actual,expected) ? "YES" : "NO");
+                Arrays.equals(actual, expected) ? "YES" : "NO");
     }
 }
--- src/Utility.java
+++ src/Utility.java
@@ -1,22 +1,21 @@
 public class Utility {
 
-    public static int[] findTwoNumbers(int[] sorted, int target)
-    {
+
+    /**
+     * Since the array is sorted already, so we can use two pointers to solve this problem.
+     * Time Complexity: O(n), where n is the size of the array
+     * Space Complexity: O(1)
+     */
+    public static int[] findTwoNumbers(int[] sorted, int target) {
         int len = sorted.length;
-        int left=0,right=len-1;
-        while(left < right)
-        {
-            if(sorted[left]+sorted[right]>target)
-            {
+        int left = 0, right = len - 1;
+        while (left < right) {
+            if (sorted[left] + sorted[right] > target) {
                 right--;
-            }
-            else if(sorted[left]+sorted[right]<target)
-            {
+            } else if (sorted[left] + sorted[right] < target) {
                 left++;
-            }
-            else
-            {
-                return new int[]{left,right};
+            } else {
+                return new int[]{left, right};
             }
         }
         return new int[]{};
